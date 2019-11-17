eae695bb34e7d00497bbb4179f9ba848e11d7618
Horacio Ramirez <horacioram94@gmail.com>
2019-11-17 22:28:29 +0000

    Added datepicker

--- src/components/userComponent/editItem/EditItem.js
+++ src/components/userComponent/editItem/EditItem.js
@@ -1,62 +1,106 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { registerLocale } from "react-datepicker";
+import es from "date-fns/locale/es";
+
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import "./EditItem.css";
 
 export default class EditModal extends Component {
   constructor(props) {
     super(props);
+    registerLocale("es", es);
 
     this.state = {
-      description: '',
+      description: "",
       price: null,
       creator: {
-        id: JSON.parse(sessionStorage.getItem("userData")).id,
+        id: JSON.parse(sessionStorage.getItem("userData")).id
       },
-      supplier: '',
+      supplier: "",
       reducedPrice: 0,
-      startDate: '',
-      endDate: ''
-      
+      startDate: new Date(),
+      endDate: new Date()
     };
     // this.formHandler = this.formHandler.bind();
   }
 
-  formHandler = (itemCode, refresh) => {
-    let data = {  
-      "itemCode": itemCode,
-      "description": this.state.description,
-      "price": this.state.price,
-      "creator": this.state.creator,
-      "suppliers": { "id": this.state.supplier },
-      "priceReduction": {
-        "reducedPrice": this.state.reducedPrice,
-        "startDate": this.state.startDate,
-        "endDate": this.state.endDate,
-      }
-    }
-
-    axios
-        .put(`http://localhost:8180/items/update`, data)
-        .then(res => {
-         refresh()
-        });
+  formattedDate = date => {
+    return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
   };
 
-  inputHandler = (e) => {
-    this.setState({
-      [e.target.name] : e.target.value
-    })
+  isCorrectDate = (startDate, endDate) => {
+    if(startDate >= endDate) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
-  render() {
+  formHandler = (itemCode, refresh) => {
 
-    let Select = (<select name="supplier" onChange={ this.inputHandler }>
-                    <option value="">---</option>
-                  {this.props.suppliers.map(supplier=> (
-                      <option key={supplier.id} value={supplier.id}>{supplier.name + ' - ' + supplier.country}</option>
-                  ))}
-   
-                </select>);
-    
+    let startDate;
+    let endDate;
+
+    if(this.isCorrectDate(this.state.startDate, this.state.endDate)) {
+      startDate = this.formattedDate(this.state.startDate);
+      endDate = this.formattedDate(this.state.endDate);
+    } else {
+      startDate = '';
+      endDate = ''
+      alert('Start date and End date have not be added because end date can not be before start date')
+    }
+
+    let data = {
+      itemCode: itemCode,
+      description: this.state.description,
+      price: this.state.price,
+      creator: this.state.creator,
+      suppliers: { id: this.state.supplier },
+      priceReduction: {
+        reducedPrice: this.state.reducedPrice,
+        startDate: startDate,
+        endDate: endDate
+      }
+    };
+
+    axios.put(`http://localhost:8180/items/update`, data).then(res => {
+      refresh();
+    });
+  };
+
+  inputHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  startDateChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  endDateChange = date => {
+    this.setState({
+      endDate: date
+    });
+  };
+
+  render() {
+    let Select = (
+      <select name="supplier" onChange={this.inputHandler}>
+        <option value="">---</option>
+        {this.props.suppliers.map(supplier => (
+          <option key={supplier.id} value={supplier.id}>
+            {supplier.name + " - " + supplier.country}
+          </option>
+        ))}
+      </select>
+    );
+
     return this.props.data.map(item => (
       <div
         key={"k1 - " + item.itemCode}
@@ -84,16 +128,52 @@ export default class EditModal extends Component {
             </div>
             <div className="modal-body">
               <div className="form-group">
-                <input type="text" name="description" placeholder="Description" onChange={ this.inputHandler } autoComplete="off"/>
-                <input type="number" name="price" placeholder="Price" onChange={ this.inputHandler } step="0.01"  autoComplete="off"/>
+                <input
+                  type="text"
+                  name="description"
+                  placeholder="Description"
+                  onChange={this.inputHandler}
+                  autoComplete="off"
+                />
+                <input
+                  type="number"
+                  name="price"
+                  placeholder="Price"
+                  onChange={this.inputHandler}
+                  step="0.01"
+                  autoComplete="off"
+                />
               </div>
               <div className="form-group">
                 {Select}
-                <input type="number" name="priceReduction" placeholder="Price reduction" onChange={ this.inputHandler } step="0.01"  autoComplete="off"/>
+                <input
+                  type="number"
+                  name="priceReduction"
+                  placeholder="Price reduction"
+                  onChange={this.inputHandler}
+                  step="0.01"
+                  autoComplete="off"
+                />
               </div>
               <div className="form-group">
-                <input type="text" name="startDate" placeholder="Start date" onChange={ this.inputHandler } autoComplete="off"/>
-                <input type="text" name="endDate" placeholder="End date" onChange={ this.inputHandler } autoComplete="off"/>
+                <div className="calendar">
+                  <label>From:</label>
+                  <DatePicker
+                    locale="es"
+                    selected={this.state.startDate}
+                    onChange={this.startDateChange}
+                    dateFormat="dd-MM-yyyy"
+                  />
+                </div>
+                <div className="calendar">
+                  <label>To:</label>
+                  <DatePicker
+                    locale="es"
+                    selected={this.state.endDate}
+                    onChange={this.endDateChange}
+                    dateFormat="dd-MM-yyyy"
+                  />
+                </div>
               </div>
             </div>
             <div className="modal-footer">
@@ -104,7 +184,13 @@ export default class EditModal extends Component {
               >
                 Close
               </button>
-              <button type="button" className="btn btn-primary" onClick={(e) => this.formHandler(item.itemCode, this.props.refreshTable)}>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={e =>
+                  this.formHandler(item.itemCode, this.props.refreshTable)
+                }
+              >
                 Save changes
               </button>
             </div>
